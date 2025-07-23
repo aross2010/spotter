80a80ead76ffd1880f505d3bf067fe9fbc077770
Alex Ross <adross1027@gmail.com>
2025-07-23 10:37:15 -0700

    migrate to supabase

--- web-server/src/index.ts
+++ web-server/src/index.ts
@@ -1,16 +1,13 @@
-import { drizzle } from 'drizzle-orm/neon-serverless'
+import { drizzle } from 'drizzle-orm/postgres-js'
 import ws from 'ws'
 import { schema } from './db/schema'
+import postgres from 'postgres'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL environment variable is not set')
 }
 
-const db = drizzle({
-  connection: process.env.DATABASE_URL,
-  ws: ws,
-  schema: schema,
-})
+const db = postgres(process.env.DATABASE_URL!)
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL environment variable is not set')
